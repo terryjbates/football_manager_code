9f6a6717d9436e803a919632cf592f406510dd0b
Terry Bates <terryjbates@gmail.com>
2023-10-30 04:38:21 -0700

    Code and data for FM player import

--- R_code/FM_Squad_Eval/global.R
+++ R_code/FM_Squad_Eval/global.R
@@ -7,16 +7,19 @@
 #  Age = c(24, 26, 22, 23, 25)
 #)
 
-# Sample data
-data_df <- data.frame(
-  Name = c("Player Abacus", "Player Bolshivek", "Player Comrade", "Player Druze", "Player Erroll"),
-  Best_Pos = c("GK", "D (C)", "AM (L)", "ST (C)", "M (C)"),
-  Height = c(180, 185, 170, 175, 182),
-  Weight = c(70, 78, 65, 72, 68),
-  Age = c(24, 26, 22, 23, 25),
-  Club = c("Kashima", "Kashima", "Kawasaki", "VentForet", "Kawasaki")
-)
+# Sample data with clubs
+#data_df <- data.frame(
+#  Name = c("Player Abacus", "Player Bolshivek", "Player Comrade", "Player Druze", "Player Erroll"),
+#  Best_Pos = c("GK", "D (C)", "AM (L)", "ST (C)", "M (C)"),
+#  Height = c(180, 185, 170, 175, 182),
+#  Weight = c(70, 78, 65, 72, 68),
+#  Age = c(24, 26, 22, 23, 25),
+#  Club = c("Kashima", "Kashima", "Kawasaki", "VentForet", "Kawasaki")
+#)
 
+# "Real" FM data read from disk
+
+data_df <- read.table("kawa_player_data.txt", header = TRUE, sep ="\t")
 
 # Create LastName to create LastName label option
 data_df$LastName <- sapply(strsplit(data_df$Name, " "), tail, 1)
---
+++ R_code/FM_Squad_Eval/import_fm_data.R
@@ -0,0 +1,17 @@
+html_file <- "test_player_shiny.html"
+kawa_webpage <- read_html(html_file)
+
+# Extract the table
+kawa_player_table <- html_nodes(kawa_webpage, "table") %>% html_table(fill = TRUE)
+
+# Convert the table to a data frame
+kawa_player_data <- kawa_player_table[[1]]
+
+# Change `Best Pos` to Best_Pos 
+kawa_player_data <- player_data %>% mutate(Best_Pos = `Best Pos`)
+
+# Save data for later
+write.table(kawa_player_data, file=kawa_player_data.txt, sep= "\t", row.names= FALSE)
+
+# To import data (in global.R)
+ df_data <- read.table("kawa_player_data.txt", header=TRUE, sep="\t")
