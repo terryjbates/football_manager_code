25195f5133554abe7ac7b76b353006a8cb872fe3
Terry Bates <terryjbates@gmail.com>
2023-11-07 01:17:16 -0800

    Add column deduplication in import_fm_data.R

--- R_code/FM_Squad_Eval/import_fm_data.R
+++ R_code/FM_Squad_Eval/import_fm_data.R
@@ -8,10 +8,16 @@ kawa_player_table <- html_nodes(kawa_webpage, "table") %>% html_table(fill = TRU
 kawa_player_data <- kawa_player_table[[1]]
 
 # Change `Best Pos` to Best_Pos 
-kawa_player_data <- player_data %>% mutate(Best_Pos = `Best Pos`)
+# kawa_player_data <- player_data %>% mutate(Best_Pos = `Best Pos`)
+kawa_player_data$Best_Pos <- kawa_player_data$`Best Pos`
+
+# Remove duplicate columns
+kawa_player_data <- kawa_player_data[!duplicated(colnames(kawa_player_data))]
 
 # Save data for later
-write.table(kawa_player_data, file=kawa_player_data.txt, sep= "\t", row.names= FALSE)
+write.table(kawa_player_data, file='kawa_player_data.txt', sep= "\t", 
+            row.names=FALSE)
 
 # To import data (in global.R)
  df_data <- read.table("kawa_player_data.txt", header=TRUE, sep="\t")
+ 
