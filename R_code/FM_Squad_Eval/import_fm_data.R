5b3780afcfe9e5234bc09a7bbdc5263e0d07896f
Terry Bates <terryjbates@gmail.com>
2023-11-07 02:51:47 -0800

    Convert height into decimal format, two precision places

--- R_code/FM_Squad_Eval/import_fm_data.R
+++ R_code/FM_Squad_Eval/import_fm_data.R
@@ -14,6 +14,37 @@ kawa_player_data$Best_Pos <- kawa_player_data$`Best Pos`
 # Remove duplicate columns
 kawa_player_data <- kawa_player_data[!duplicated(colnames(kawa_player_data))]
 
+# Convert height into CM
+# Define a function to convert height from feet and inches to centimeters
+ft_to_cm <- function(ft, inch) {
+  # Convert feet and inches to centimeters
+  cm <- (ft * 12 + inch) * 2.54
+  # Return the result
+  return(cm)
+}
+
+# Split the height column into feet and inches
+height_split <- strsplit(kawa_player_data[['Height']], "'")
+
+# Extract the feet and inches values
+feet <- sapply(height_split, function(x) as.numeric(x[1]))
+inches <- sapply(height_split, function(x) as.numeric(gsub('"', '', x[2])))
+
+
+# Convert the height to centimeters
+height_cm <- ft_to_cm(feet, inches)
+
+# Define a function to convert from centimeters to fractional Imperial
+cm_to_ft <- function(cm) {
+  # Convert feet and inches to centimeters
+  ft <-  round((cm / 30.48), 2)
+  # Return the result
+  return(ft)
+}
+
+# Overwrite the Height data
+kawa_player_data[['Height']] <- cm_to_ft(height_cm) 
+
 # Save data for later
 write.table(kawa_player_data, file='kawa_player_data.txt', sep= "\t", 
             row.names=FALSE)
